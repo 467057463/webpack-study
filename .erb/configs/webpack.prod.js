18f50ed060c1a5abca5971d657a4f015fa0d451f
m2 <467057463@qq.com>
2021-06-26 14:49:10 +0800

    change

--- .erb/configs/webpack.prod.js
+++ .erb/configs/webpack.prod.js
@@ -3,32 +3,32 @@ const baseConfig = require('./webpack.base');
 const path = require('path')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-const TerserPlugin = require("terser-webpack-plugin");
-const CompressionPlugin = require("compression-webpack-plugin");
-const Dotenv = require('dotenv-webpack');
+// const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// const TerserPlugin = require("terser-webpack-plugin");
+// const CompressionPlugin = require("compression-webpack-plugin");
+// const Dotenv = require('dotenv-webpack');
 
 
-const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
-const smp = new SpeedMeasurePlugin();
+// const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
+// const smp = new SpeedMeasurePlugin();
 
 const config =  merge(baseConfig, {
   plugins: [
     new MiniCssExtractPlugin({
       filename: 'css/[name]_[fullhash:6].css'
     }),
-    new BundleAnalyzerPlugin(),
-    new Dotenv({
-      defaults: path.resolve(__dirname, './.env'),
-      path: path.resolve(__dirname, './.env.production')
-    }),
+    // new BundleAnalyzerPlugin(),
+    // new Dotenv({
+    //   defaults: path.resolve(__dirname, './.env'),
+    //   path: path.resolve(__dirname, './.env.production')
+    // }),
   ],
   optimization: {
     minimize: true,
     minimizer: [
-      new TerserPlugin(),
+      // new TerserPlugin(),
       new CssMinimizerPlugin(),
-      new CompressionPlugin()
+      // new CompressionPlugin()
     ]
   }
 })
